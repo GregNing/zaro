041ab7f6f8b68b12cc41d4eb54572bdbbcf8ad8b
Greg <GregJieNing@gmail.com>
2018-03-03 00:10:07 +0800

    add assam

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,14 +26,14 @@ class OrdersController < ApplicationController
     @product_lists = @order.product_lists
   end
   def pay_with_alipay      
-      @order.set_payment_with!("alipay")
-      @order.pay!
+      @order.set_payment_with!("alipay")      
+      @order.make_payment!
       redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
   end
 
   def pay_with_wechat    
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.make_payment!
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
   end
   private
