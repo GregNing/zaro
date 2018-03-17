64866b5dd0f1ffa1587a70a0b47a9883137b9a6f
Greg <GregJieNing@gmail.com>
2018-03-18 02:20:51 +0800

    改變回船隻庫存量

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,7 +5,7 @@
 #  id         :integer          not null, primary key
 #  cart_id    :integer
 #  product_id :integer
-#  quantity   :integer          default(1)
+#  quantity   :integer          default(0)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  size       :string
@@ -18,5 +18,18 @@ class CartItem < ApplicationRecord
     def change_quantity!(quantity)        
         self.quantity += quantity
         self.save!        
+    end
+    #輸入 size 回傳成 quantity(庫存量) ex: input "S" puts 5
+    def size_quantity_to_change(size)
+        #轉所需的hash
+        @size = eval(":#{size.to_s.downcase}")
+        @result = 0
+        if self.quantity.present?
+            #將庫存量 text 轉 hash
+            @quantity = eval(self.quantity)
+            #將size 也轉為hash
+            @result = @quantity[@size]
+        end
+        return @result
     end
 end
